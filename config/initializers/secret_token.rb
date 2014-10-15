01c20d3a5593b9915d4418b53b79fff86df0b439
Cory Diamand <cory@postpourri.com>
2014-10-15 17:38:49 -0400

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lineitems::Application.config.secret_key_base = '5bdce2b2bfddd4188421f638718f0dbbb3af033011896ac03dfae5b482921923886e6e2341dde2814791846c491ff8170eed288f560ff809b156444ed2fdacfd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Lineitems::Application.config.session_store :cookie_store, key: '_lineitems_session'
