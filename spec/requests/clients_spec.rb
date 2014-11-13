90bfefda7e4598bedcd1780592210cb7a625d106
Cory Diamand <cory@postpourri.com>
2014-11-13 00:44:35 -0500

    pending request specs

--- spec/requests/clients_spec.rb
+++ spec/requests/clients_spec.rb
@@ -6,13 +6,16 @@ Warden.test_mode!
 RSpec.describe "Clients", :type => :request do
 
 	admin = FactoryGirl.create(:admin, :email => "rando123@rando.com")
-	login_as(admin, :scope => :admin)
 
 	describe "GET /clients" do
+
 		it "works! (now write some real specs)" do
+			pending
 			get clients_path
 			expect(response.status).to eq(200)
 			#expect(response.status).to eq(302)
 		end
 	end
+
+
 end
