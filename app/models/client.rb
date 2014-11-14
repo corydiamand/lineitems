dc907c7f3fa900a51d7a277634844ec2e0489d86
Cory Diamand <cory@postpourri.com>
2014-11-13 23:43:59 -0500

    client permalink is nil by default and cant be solely numbers

--- app/models/client.rb
+++ app/models/client.rb
@@ -17,5 +17,19 @@
 class Client < ActiveRecord::Base
 	validates :first_name, :last_name, presence: true
 	validates :permalink, uniqueness: {allow_blank: true}
+	validate  :permalink, :permalink_not_id
+
 	after_initialize 'self.page_visible ||= false'
+	#set to nil so that blank permalink doesn't lead to clients unless intended
+	after_initialize 'self.permalink ||= nil'
+
+	#so that permalinks will never get mixed up with an id
+	private
+
+		def permalink_not_id
+			#regex checks if permalink contains only numbers
+			if (self.permalink !~ /\D/) == true && self.permalink != nil
+				errors.add(:permalink, "permalink can't be just numbers, add a letter")
+			end
+		end
 end
